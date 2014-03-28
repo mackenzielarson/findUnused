6b20b515363525263ad5caaf7b6c6cf786b561f8
Mackenzie Larson <larsonma@seawolf.sonoma.edu>
2014-03-27 17:12:05 -0700

    Updated .sh file with modified code

--- findUnused.sh
+++ findUnused.sh
@@ -11,28 +11,23 @@
 # Look for image files within the sample directory inside of the current working directory.
 # Find each image file types individually and save it in /tmp/patterns.
 
-echo ' '
-echo "Files not referenced in directory."
-echo ' '
 #finds all files with name.jpg .png and .gif puts into temporary file  "patterns"
-#basename removes suffixes
-imagelist=$(find . jpg);
-echo $imagelist;
-echo "===="
-for image in $imagelist; do
-    echo "my image $image"
-done;
-# find . -name *.png -exec basename {} \; >> /tmp/patterns
-# find . -name *.gif -exec basename {} \; >> /tmp/patterns
+
+fileList=$(find . | grep -E ".+(txt|json|xml)")
+imagelist=$(find . | grep -E ".+(jpg|png|gif|jpeg)");
+
 
 # Print a list of files that DO NOT exist.
 # The variable 'p' is a representation of /tmp/patterns.
-# /dev/null removes any files that exist in a directory but are not referenced by any files in the curret directory.
-# The cat command removes coloring.
 
-for p in $(cat /tmp/patterns); do
-    grep -R $p . > /dev/null || echo $p;
+for image in $imagelist; 
+do
+    grep -R -q $image $filelist;
+    if [ $? -eq 1 ]; then
+       echo $image;
+    fi
+
 done
-echo ' '
+
 
 
